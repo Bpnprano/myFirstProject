90f02517469427a415e64453f2422395c9b3230e
DELL <DELL@DESKTOP-QUBD303>
2021-04-25 10:29:58 +0530

    My_fisrt_project_addition

---
+++ main.c
@@ -0,0 +1,19 @@
+
+
+/**
+ * main.c
+ */
+int  addition(int A, int B);
+int main(void)
+{
+    int additionvalue=0;
+    additionvalue = addition(5,7);
+	return 0;
+}
+
+int addition(int A, int B)
+{
+int c=0;
+c= A+B;
+return c;
+}
